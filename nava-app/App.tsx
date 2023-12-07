2546b57dccf7d33bf3e81d933ee9f4b9258c569d
Bwalya <gabrielmaele1@yahoo.com>
2023-12-07 04:47:43 -0600

    Native wind & Tailwind CSS
    Setting Up Native Wind & Tailwind CSS & Babel Plugin

--- nava-app/App.tsx
+++ nava-app/App.tsx
@@ -1,9 +1,10 @@
+// View is the same thing as <dev> 
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className='p-10'>
+    <View >
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
